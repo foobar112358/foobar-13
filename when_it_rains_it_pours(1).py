a65c45b9557dbc3706a42b5688ad4cd66c553ab5
lpleahy <lpleahy@berkeley.edu>
2015-08-18 02:12:05 -0400

    first time attempt

--- when_it_rains_it_pours(1).py
+++ when_it_rains_it_pours(1).py
@@ -1,30 +1,22 @@
-import copy
+import copy, heapq
 
 __author__ = 'Liam'
 
 
-def all_same(heights):
-    i = heights[0]
-    for j in heights:
-        if i != j:
-            return False
-    return True
-
-
 def answer(heights):
     """
-    # >>> answer([1, 4, 2, 5, 1, 2, 3])
-    # 5
-    # >>> answer([1, 2, 3, 2, 1])
-    # 0
-    # >>> answer([2,2,2,2,2,2,2,2])
-    # 0
-    # >>> answer([1,2,3,3,2,3,4,5,6])
-    # 1
-    # >>> answer([0])
-    # 0
-    # >>> answer([1,2,3,4,5,6,7,8,7,9])
-    # 1
+    >>> answer([1, 4, 2, 5, 1, 2, 3])
+    5
+    >>> answer([1, 2, 3, 2, 1])
+    0
+    >>> answer([2,2,2,2,2,2,2,2])
+    0
+    >>> answer([1,2,3,3,2,3,4,5,6])
+    1
+    >>> answer([0])
+    0
+    >>> answer([1,2,3,4,5,6,7,8,7,9])
+    1
     >>> answer([1,2,1,2,1,2,1,2,1])
     3
     """
@@ -33,7 +25,7 @@ def answer(heights):
     counter = 0
     i = heights.index(max(heights))
     max_val = heights[i]
-    j = heights.index(max(height for height in heights if heights.index(height) != heights.index(i)))
+    j = get_other(heights, i)
     counter += fill_in_matrix(heights, i, j)
     return counter + answer(heights[:min(i, j) + 1]) + answer(heights[max(i, j):])
 
@@ -47,3 +39,17 @@ def fill_in_matrix(matrix, i, j):
     for k in range(min(i, j) + 1, max(i, j)):
         counter += abs(maxi - matrix[k])
     return counter
+
+
+def all_same(heights):
+    i = heights[0]
+    for j in heights:
+        if i != j:
+            return False
+    return True
+
+
+def get_other(heights, i):
+    ref = copy.copy(heights)
+    ref[i] = 0
+    return ref.index(max(ref))
